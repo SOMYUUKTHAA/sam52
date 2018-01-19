f23ada6e030a782d7e2ea6939ce11cb04f089ef9
SOMYUUKTHAA <30828746+SOMYUUKTHAA@users.noreply.github.com>
2018-01-19 11:14:41 +0000

    Create sam52.C

---
+++ sam52.C
@@ -0,0 +1,33 @@
+#include<stdio.h>
+void main()
+{
+    int N;
+    printf("ENTER ANY VALUE");
+    scanf("\t%d",&N);
+    if(N<=10)
+    {
+        switch(0)
+        {
+        case 1:"one";
+        break;
+        case 2:"two";
+        break;
+        case 3:"three";
+        break;
+        case 4:"four";
+        break;
+        case 5:"five";
+        break;
+        case 6:"six";
+        break;
+        case 7:"seven";
+        break;
+        case 8:"eight";
+        break;
+        case 9:"nine";
+        break;
+        case 10:"ten";
+        break;
+        }
+    }
+}
